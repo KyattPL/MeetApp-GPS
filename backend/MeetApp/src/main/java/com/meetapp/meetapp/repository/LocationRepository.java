3320d293fa81ee928d1547880fc48aece3205bab
Kajetan Pynka <siema1234@gazeta.pl>
2023-03-20 16:09:47 +0100

    Added findWithin method to LocationRepository

--- backend/MeetApp/src/main/java/com/meetapp/meetapp/repository/LocationRepository.java
+++ backend/MeetApp/src/main/java/com/meetapp/meetapp/repository/LocationRepository.java
@@ -1,7 +1,9 @@
 package com.meetapp.meetapp.repository;
 
 import com.meetapp.meetapp.model.Location;
+import org.locationtech.jts.geom.Geometry;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,4 +14,7 @@ public interface LocationRepository extends JpaRepository<Location, Integer> {
     Optional<Location> findByCityNameAndVoivodeshipName(String cityName, String voivodeshipName);
 
     List<Location> findDistinctTop10ByCityNameContainingIgnoreCaseOrderById(String cityName);
+
+    @Query("select l from Location l where within(l.point,:filter) = true")
+    List<Location> findWithin(Geometry filter);
 }
