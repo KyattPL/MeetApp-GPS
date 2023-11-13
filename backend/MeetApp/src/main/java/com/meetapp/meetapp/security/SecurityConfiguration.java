2a040cd682e8159cc7badc42a982f0af546220f1
KyattPL <siema1234@gazeta.pl>
2023-11-13 12:31:18 +0100

    Maybe idk

--- backend/MeetApp/src/main/java/com/meetapp/meetapp/security/SecurityConfiguration.java
+++ backend/MeetApp/src/main/java/com/meetapp/meetapp/security/SecurityConfiguration.java
@@ -1,13 +1,23 @@
 package com.meetapp.meetapp.security;
 
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContextBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.web.client.RestTemplate;
+
+import javax.net.ssl.SSLContext;
+import java.io.File;
+import java.net.http.HttpClient;
 
 @Configuration
 @EnableWebSecurity
@@ -27,5 +37,19 @@ public class SecurityConfiguration {
                 .and().cors().disable().csrf().disable();
 
         return http.build();
+    }
+
+    @Bean
+    RestTemplate restTemplate() throws Exception {
+        SSLContext sslContext = new SSLContextBuilder()
+                .loadTrustMaterial(new File("classpath:springboot.pk12"), "password".toCharArray())
+                .build();
+        SSLConnectionSocketFactory socketFactory = new SSLConnectionSocketFactory(sslContext);
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setSSLSocketFactory(socketFactory)
+                .build();
+        HttpComponentsClientHttpRequestFactory factory =
+                new HttpComponentsClientHttpRequestFactory(httpClient);
+        return new RestTemplate(factory);
     }
 }
