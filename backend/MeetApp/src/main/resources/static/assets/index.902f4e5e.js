cb1642ed188f38e04737e52a32bdfe3092d3e775
Kajetan Pynka <siema1234@gazeta.pl>
2023-12-27 14:41:18 +0100

    Changed tooltip to hidden in group-hover

---
+++ backend/MeetApp/src/main/resources/static/assets/index.902f4e5e.js
@@ -0,0 +1,40 @@
+import{S as y,i as l,s as p,l as c,m as u,n as d,o as s,a as m,p as b,q as r,y as k,b as h,t as j,d as g,v as f}from"./index.71ef7177.js";import{H as x}from"./Header.120bd687.js";import"./stores.daae5b15.js";import"./fetchWrapper.486a233b.js";function M(w){let e,o,t,i,n;return o=new x({}),{c(){e=c("div"),u(o.$$.fragment),t=d(),i=c("div"),i.innerHTML=`<h1 class="text-3xl text-cocoa mb-2">Zasady MeetApp</h1> 
+        <p class="text-base text-cocoa mb-2">Celem MeetApp jest stworzenie globalnej przestrzeni do rozmowy. Przemoc, n\u0119kanie i podobne zachowania zniech\u0119caj\u0105 ludzi do wyra\u017Cania
+            siebie i w rezultacie zmniejszaj\u0105 warto\u015B\u0107 globalnej publicznej rozmowy. Nasze Zasady maj\u0105 zapewni\u0107 naszym u\u017Cytkownikom swobodny i
+            bezpieczny udzia\u0142 w publicznej rozmowie.</p> 
+        <h2 class="text-2xl text-cocoa mb-2">Bezpiecze\u0144stwo</h2> 
+        <p class="text-base text-cocoa mb-2">Przemoc: niedozwolone jest gro\u017Cenie u\u017Cyciem przemocy \u017Cadnej osobie ani grupie os\xF3b. Niedozwolone jest r\xF3wnie\u017C wyra\u017Canie pochwa\u0142y dla
+            przemocy. Terroryzm / brutalny ekstremizm: niedozwolone jest gro\u017Cenie aktami terroryzmu lub brutalnego ekstremizmu ani ich promowanie.
+            Dzieci\u0119ca pornografia: na MeetApp niedozwolone jest udost\u0119pnianie dzieci\u0119cej pornografii pod \u017Cadn\u0105 postaci\u0105. Nadu\u017Cycia/n\u0119kanie:
+            niedozwolone jest anga\u017Cowanie si\u0119 w ukierunkowane n\u0119kanie innych os\xF3b ani nak\u0142anianie kogokolwiek do takich dzia\u0142a\u0144. Obejmuje to wyra\u017Canie
+            pragnienia lub \u017Cyczenia by kto\u015B do\u015Bwiadczy\u0142 fizycznej krzywdy. Zachowanie nacechowane nienawi\u015Bci\u0105: niedozwolone jest promowanie przemocy
+            wobec innych, gro\u017Cenie im ani ich n\u0119kanie z powodu rasy, przynale\u017Cno\u015Bci etnicznej, kasty, pochodzenia, orientacji seksualnej, p\u0142ci,
+            to\u017Csamo\u015Bci p\u0142ciowej, religii, wieku, niepe\u0142nosprawno\u015Bci czy choroby. Osoby odpowiedzialne za brutalne akty przemocy: konta nale\u017C\u0105ce do
+            os\xF3b odpowiedzialnych za ataki terrorystyczne lub inne brutalne i masowe akty przemocy na tle ideologicznym zostan\u0105 usuni\u0119te. Dotyczy to
+            r\xF3wnie\u017C post\xF3w wyra\u017Caj\u0105cych radykalne pogl\u0105dy tych os\xF3b i innych utworzonych przez nich tre\u015Bci. Samob\xF3jstwo lub samookaleczenie:
+            niedozwolone jest promowanie samob\xF3jstwa lub samookaleczenia ani zach\u0119canie do tych czyn\xF3w. Nieodpowiednie tre\u015Bci, w tym przemoc graficzna
+            i tre\u015Bci dla doros\u0142ych: niedozwolone jest publikowanie nadmiernie brutalnych multimedi\xF3w ani udost\u0119pnianie tre\u015Bci przepe\u0142nionych przemoc\u0105
+            lub tre\u015Bci dla doros\u0142ych w ramach transmisji na \u017Cywo, w zdj\u0119ciach profilowych lub nag\u0142\xF3wkach. Zabrania si\u0119 r\xF3wnie\u017C udost\u0119pniania
+            multimedi\xF3w przedstawiaj\u0105cych przemoc/napa\u015B\u0107 seksualn\u0105. Nielegalne lub regulowane prawnie towary lub us\u0142ugi: niedozwolone jest korzystanie
+            z naszej us\u0142ugi w jakimkolwiek niezgodnym z prawem celu lub w celu wspierania nielegalnych aktywno\u015Bci. Obejmuje to sprzeda\u017C, kupowanie lub
+            u\u0142atwianie transakcji dotycz\u0105cych nielegalnych towar\xF3w lub us\u0142ug, a tak\u017Ce niekt\xF3rych rodzaj\xF3w regulowanych towar\xF3w lub us\u0142ug.</p> 
+        <h2 class="text-2xl text-cocoa mb-2">Prywatno\u015B\u0107</h2> 
+        <p class="text-base text-cocoa mb-2">Informacje osobiste: niedozwolone jest ujawnianie ani publikowanie informacji osobistych innych os\xF3b (takich jak prywatny numer telefonu i
+            adres) bez ich upowa\u017Cnienia i wyra\u017Anej zgody. Zabrania si\u0119 r\xF3wnie\u017C gro\u017Cenia ujawnieniem informacji osobistych lub zach\u0119cania innych do
+            tego. Prywatne materia\u0142y: niedozwolone jest publikowanie czy udost\u0119pnianie prywatnych zdj\u0119\u0107 lub film\xF3w przedstawiaj\u0105cych osob\u0119 trzeci\u0105,
+            kt\xF3re zosta\u0142y utworzone lub rozpowszechnione bez jej wyra\u017Anej zgody.</p> 
+        <h2 class="text-2xl text-cocoa mb-2">Autentyczno\u015B\u0107</h2> 
+        <p class="text-base text-cocoa mb-2">Manipulowanie platform\u0105 MeetApp i spam: niedozwolone jest korzystanie z us\u0142ug MeetApp w spos\xF3b sztucznie zwi\u0119kszaj\u0105cy lub ograniczaj\u0105cy
+            przep\u0142yw informacji ani anga\u017Cowanie si\u0119 w zachowania, kt\xF3re modyfikuj\u0105 lub zak\u0142\xF3caj\u0105 spos\xF3b korzystania z MeetApp przez innych
+            u\u017Cytkownik\xF3w. Integralno\u015B\u0107 obywatelska: niedozwolone jest korzystanie z us\u0142ug MeetApp w celu wp\u0142ywania na wybory czy inne procesy
+            obywatelskie lub ich zak\u0142\xF3cania. Obejmuje to publikowanie lub udost\u0119pnianie tre\u015Bci, kt\xF3re mog\u0105 uniemo\u017Cliwi\u0107 uczestniczenie w procesie
+            obywatelskim lub wprowadza\u0107 w b\u0142\u0105d co do tego, kiedy, gdzie lub w jaki spos\xF3b mo\u017Cna w nim uczestniczy\u0107. Wprowadzanie innych u\u017Cytkownik\xF3w w
+            b\u0142\u0105d co do swojej to\u017Csamo\u015Bci: niedozwolone jest podszywanie si\u0119 pod inne osoby, grupy lub organizacje w celu wprowadzenia w b\u0142\u0105d,
+            zwiedzenia lub oszukania innych, w tym pos\u0142ugiwanie si\u0119 fa\u0142szyw\u0105 to\u017Csamo\u015Bci\u0105 w spos\xF3b utrudniaj\u0105cy innym korzystanie z MeetApp.
+            Nieautentyczne i edytowane multimedia: niedozwolone jest udost\u0119pnianie nieautentycznych czy edytowanych multimedi\xF3w, kt\xF3rych tre\u015B\u0107 mog\u0142aby
+            komu\u015B zaszkodzi\u0107, bez wyra\u017Anego oznaczenia ich rodzaju. Ponadto mo\u017Cemy oznacza\u0107 posty zawieraj\u0105ce edytowane i nieautentyczne multimedia,
+            aby pom\xF3c u\u017Cytkownikom zrozumie\u0107 ich pochodzenie i zapewni\u0107 dodatkowy kontekst. Prawa autorskie i znaki towarowe: niedozwolone jest
+            naruszanie praw w\u0142asno\u015Bci intelektualnej innych os\xF3b, w tym praw autorskich i znak\xF3w towarowych.</p> 
+        <h2 class="text-2xl text-cocoa mb-2">Reklamy stron trzecich zawarte w tre\u015Bciach wideo</h2> 
+        <p class="text-base text-cocoa mb-2">Nie wolno przesy\u0142a\u0107, udost\u0119pnia\u0107 ani wy\u015Bwietla\u0107 w naszych serwisach \u017Cadnych tre\u015Bci wideo zawieraj\u0105cych reklamy stron trzecich, takie jak
+            reklamy w trybie \u201Epre-roll\u201D lub sponsorowane tre\u015Bci graficzne, bez naszej uprzedniej zgody.</p>`,s(i,"class","h-[calc(100%-4rem)] lg:h-[calc(100%-8rem)] overflow-auto p-4"),s(e,"class","h-screen")},m(a,z){m(a,e,z),b(o,e,null),r(e,t),r(e,i),n=!0},p:k,i(a){n||(h(o.$$.fragment,a),n=!0)},o(a){j(o.$$.fragment,a),n=!1},d(a){a&&g(e),f(o)}}}class P extends y{constructor(e){super(),l(this,e,null,M,p,{})}}export{P as default};
