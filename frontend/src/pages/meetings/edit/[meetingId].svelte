af7b8a3835c5fc6a1c1696e06fda5cb2696b82b8
Kajetan Pynka <siema1234@gazeta.pl>
2024-01-07 22:58:40 +0100

    Okay, it SHOULD work

--- frontend/src/pages/meetings/edit/[meetingId].svelte
+++ frontend/src/pages/meetings/edit/[meetingId].svelte
@@ -78,8 +78,9 @@
             title = r.title;
 
             peopleLimitValue = r.personQuota === null ? null : r.personQuota;
-            dateValue = new Date(r.meetingDateTime.date.replace('.', '-'));
-            timeValue = new Date(r.meetingDateTime.date.replace('.', '-') + 'T' + r.meetingDateTime.time);
+            let correctDate = r.meetingDateTime.date.replaceAll('.', '-').split('-').reverse().join('-');
+            dateValue = new Date(correctDate);
+            timeValue = new Date(correctDate + 'T' + r.meetingDateTime.time);
         })
         .then(() => execute('categories', 'GET').then(async (cats) => (categories = await cats.json())));
 
